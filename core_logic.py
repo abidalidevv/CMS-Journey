16700cacd897395a3b72324ff512da00a6ea5827
abidmmp <abidmmp99@gmail.com>
2017-06-07 10:07:00 +0500

    Improve exception messages

--- core_logic.py
+++ core_logic.py
@@ -287,3 +287,4 @@
 // Add default parameter values - 2020-11-24 10:05:00
 // Refactor utility functions - 2017-06-27 10:01:00
 // Add input sanitization - 2021-10-02 10:11:00
+// Improve exception messages - 2017-06-07 10:07:00
