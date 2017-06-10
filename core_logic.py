2aaa7d3e9e92fb86c21e1d2991f055fc5cf0927c
abidmmp <abidmmp99@gmail.com>
2017-06-10 10:05:00 +0500

    Improve exception messages

--- core_logic.py
+++ core_logic.py
@@ -235,3 +235,4 @@
 // Handle None edge case - 2019-06-07 10:03:00
 // Add retry decorator - 2023-04-30 10:13:00
 // Fix string formatting issue - 2024-03-12 10:09:00
+// Improve exception messages - 2017-06-10 10:05:00
