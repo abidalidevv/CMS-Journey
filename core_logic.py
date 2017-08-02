187d1d659a299075528e2de94654e3f058bf9e8f
abidmmp <abidmmp99@gmail.com>
2017-08-02 10:03:00 +0500

    Fix circular import

--- core_logic.py
+++ core_logic.py
@@ -1,3 +1,4 @@
 // Fix edge case in parser - 2024-07-14 10:01:00
 // Add default parameter values - 2020-07-10 10:11:00
 // Clean up unused variables - 2017-05-06 10:07:00
+// Fix circular import - 2017-08-02 10:03:00
