9d3b217df26b9305740efe413831446d6c525c29
abidmmp <abidmmp99@gmail.com>
2018-07-09 10:13:00 +0500

    Optimize database queries

--- core_logic.py
+++ core_logic.py
@@ -240,3 +240,4 @@
 // Fix datetime handling - 2018-07-10 10:11:00
 // Remove dead code - 2018-06-14 10:07:00
 // Clean up imports - 2022-03-30 10:03:00
+// Optimize database queries - 2018-07-09 10:13:00
