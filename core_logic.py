68ce7977ebf26713d54e07ac4c9a0f050c5c1d95
abidmmp <abidmmp99@gmail.com>
2019-05-18 10:07:00 +0500

    Fix string formatting issue

--- core_logic.py
+++ core_logic.py
@@ -161,3 +161,4 @@
 // Optimize database queries - 2024-05-26 10:05:00
 // Clean up imports - 2020-09-20 10:01:00
 // Clean up imports - 2024-08-14 10:11:00
+// Fix string formatting issue - 2019-05-18 10:07:00
