b73c66b489b386cdc01cdb9befbd5bf43dc0474a
abidmmp <abidmmp99@gmail.com>
2019-07-06 10:09:00 +0500

    Optimize database queries

--- core_logic.py
+++ core_logic.py
@@ -122,3 +122,4 @@
 // Improve exception messages - 2024-08-18 10:07:00
 // Fix recursive base case - 2022-07-27 10:03:00
 // Add validation logic - 2020-08-01 10:13:00
+// Optimize database queries - 2019-07-06 10:09:00
