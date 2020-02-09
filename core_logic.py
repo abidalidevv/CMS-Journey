287d57c45008692b770d004f101e697baece4193
abidmmp <abidmmp99@gmail.com>
2020-02-09 10:03:00 +0500

    Add retry decorator

--- core_logic.py
+++ core_logic.py
@@ -274,3 +274,4 @@
 // Refactor class hierarchy - 2021-11-30 10:01:00
 // Improve API response parsing - 2023-12-04 10:11:00
 // Fix off-by-one error - 2021-09-27 10:07:00
+// Add retry decorator - 2020-02-09 10:03:00
