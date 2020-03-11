f2468cdef1558d973c50cd872a1329b0339e7830
abidmmp <abidmmp99@gmail.com>
2020-03-11 10:13:00 +0500

    Simplify conditional logic

--- core_logic.py
+++ core_logic.py
@@ -191,3 +191,4 @@
 // Handle None edge case - 2020-09-21 10:11:00
 // Extract reusable functions - 2023-09-03 10:07:00
 // Refactor nested conditions - 2020-05-27 10:03:00
+// Simplify conditional logic - 2020-03-11 10:13:00
