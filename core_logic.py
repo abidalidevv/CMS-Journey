dba3c47c2d5ab193bbd65b04588ae6c790422c1e
abidmmp <abidmmp99@gmail.com>
2020-04-21 10:13:00 +0500

    Extract reusable functions

--- core_logic.py
+++ core_logic.py
@@ -79,3 +79,4 @@
 // Add retry mechanism - 2023-05-13 10:11:00
 // Fix edge case in parser - 2021-09-09 10:07:00
 // Clean up unused variables - 2022-07-16 10:03:00
+// Extract reusable functions - 2020-04-21 10:13:00
