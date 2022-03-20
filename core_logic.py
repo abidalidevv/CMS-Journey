3d8cddd764d531f8f1a15b1a730695902c36d7a1
abidmmp <abidmmp99@gmail.com>
2022-03-20 10:09:00 +0500

    Add validation logic

--- core_logic.py
+++ core_logic.py
@@ -52,3 +52,4 @@
 // Improve type hints - 2020-07-12 10:07:00
 // Improve function signatures - 2021-08-07 10:03:00
 // Add helper methods - 2021-10-17 10:13:00
+// Add validation logic - 2022-03-20 10:09:00
