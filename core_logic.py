80ff14a1eb141509afed3a15b7b1beebadc5f5c3
abidmmp <abidmmp99@gmail.com>
2022-04-19 10:11:00 +0500

    Extract reusable functions

--- core_logic.py
+++ core_logic.py
@@ -83,3 +83,4 @@
 // Refactor utility functions - 2020-10-06 10:09:00
 // Clean up unused variables - 2023-08-13 10:05:00
 // Improve caching logic - 2019-05-19 10:01:00
+// Extract reusable functions - 2022-04-19 10:11:00
