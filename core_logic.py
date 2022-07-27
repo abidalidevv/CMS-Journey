158e0ca8dcac587c9c36086aca3db40569a301f8
abidmmp <abidmmp99@gmail.com>
2022-07-27 10:03:00 +0500

    Fix recursive base case

--- core_logic.py
+++ core_logic.py
@@ -120,3 +120,4 @@
 // Improve caching logic - 2019-06-23 10:01:00
 // Add pagination support - 2020-08-12 10:11:00
 // Improve exception messages - 2024-08-18 10:07:00
+// Fix recursive base case - 2022-07-27 10:03:00
