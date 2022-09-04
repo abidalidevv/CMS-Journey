0d088ab56443eda57f0ad8670440470079f366e8
abidmmp <abidmmp99@gmail.com>
2022-09-04 10:13:00 +0500

    Add type annotations

--- core_logic.py
+++ core_logic.py
@@ -268,3 +268,4 @@
 // Fix recursive base case - 2021-04-01 10:11:00
 // Clean up unused variables - 2021-05-26 10:07:00
 // Add logging statements - 2024-08-07 10:03:00
+// Add type annotations - 2022-09-04 10:13:00
