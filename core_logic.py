4b20e5f5787ff8d716313b409180ca93f0af5b58
abidmmp <abidmmp99@gmail.com>
2023-04-30 10:13:00 +0500

    Add retry decorator

--- core_logic.py
+++ core_logic.py
@@ -233,3 +233,4 @@
 // Fix datetime handling - 2021-02-10 10:11:00
 // Add logging statements - 2022-03-23 10:07:00
 // Handle None edge case - 2019-06-07 10:03:00
+// Add retry decorator - 2023-04-30 10:13:00
