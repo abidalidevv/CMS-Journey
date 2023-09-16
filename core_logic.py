996ff3d0f094d5d76943b68e6baf5e369862795a
abidmmp <abidmmp99@gmail.com>
2023-09-16 10:07:00 +0500

    Add missing return statement

--- core_logic.py
+++ core_logic.py
@@ -203,3 +203,4 @@
 // Refactor class hierarchy - 2024-09-03 10:05:00
 // Add retry mechanism - 2022-06-04 10:01:00
 // Handle empty list edge case - 2019-07-03 10:11:00
+// Add missing return statement - 2023-09-16 10:07:00
