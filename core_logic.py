8a2ee1619c3fea682d8e720395ce07b65ce1ad9b
abidmmp <abidmmp99@gmail.com>
2023-11-08 10:11:00 +0500

    Handle empty list edge case

--- core_logic.py
+++ core_logic.py
@@ -139,3 +139,4 @@
 // Improve error handling - 2017-10-05 10:09:00
 // Fix attribute access bug - 2024-03-11 10:05:00
 // Refactor utility functions - 2022-09-22 10:01:00
+// Handle empty list edge case - 2023-11-08 10:11:00
