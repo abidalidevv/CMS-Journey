409814a0a0c24d3804cdf2f6fd76dcad3ded9d16
abidmmp <abidmmp99@gmail.com>
2024-05-25 10:03:00 +0500

    Improve error handling

--- core_logic.py
+++ core_logic.py
@@ -183,3 +183,4 @@
 // Update docstrings - 2024-11-08 10:01:00
 // Update docstrings - 2024-04-20 10:11:00
 // Add default parameter values - 2021-09-15 10:07:00
+// Improve error handling - 2024-05-25 10:03:00
