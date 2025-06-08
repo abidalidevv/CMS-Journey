3c98b71e4b21cbe10a9e976fb1793466f47975a3
Abid Ali <abidmmp99@gmail.com>
2025-06-08 16:45:42 +0000

    Add default parameter values

--- core_logic.py
+++ core_logic.py
@@ -301,3 +301,10 @@ def slugify(text):
 
 def format_date(dt):
     return dt.strftime('%Y-%m-%d')
+
+def memoize(fn):
+    cache = {}
+    def wrapper(*args):
+        if args not in cache: cache[args] = fn(*args)
+        return cache[args]
+    return wrapper
